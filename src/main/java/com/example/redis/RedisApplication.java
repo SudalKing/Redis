6fe2258ca6d4c86c0ae061c37f0694463c674be0
SudalKing <jmjang88@naver.com>
2023-11-19 22:48:22 +0900

    Pub/Sub 패턴

--- src/main/java/com/example/redis/RedisApplication.java
+++ src/main/java/com/example/redis/RedisApplication.java
@@ -1,15 +1,24 @@
 package com.example.redis;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
-@EnableCaching
 @SpringBootApplication
-public class RedisApplication {
+public class RedisApplication implements CommandLineRunner {
+
+    @Autowired
+    private ChatService chatService;
 
     public static void main(String[] args) {
         SpringApplication.run(RedisApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Application started..");
+
+        chatService.enterChatRoom("chat1");
+    }
 }
