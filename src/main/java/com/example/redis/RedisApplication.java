294408c5f35b14a94a0518c231e2a2356a48ee5d
SudalKing <jmjang88@naver.com>
2023-11-25 22:27:06 +0900

    Redis 백업

--- src/main/java/com/example/redis/RedisApplication.java
+++ src/main/java/com/example/redis/RedisApplication.java
@@ -6,19 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RedisApplication implements CommandLineRunner {
-
-    @Autowired
-    private ChatService chatService;
-
+public class RedisApplication {
     public static void main(String[] args) {
         SpringApplication.run(RedisApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("Application started..");
-
-        chatService.enterChatRoom("chat1");
     }
 }
